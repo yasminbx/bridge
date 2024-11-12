5499d90c19f79c5d55cc6581ceb1576dce0c93ab
Yasmin Belhout <yasmin.belhout@metropolia.fi>
2024-11-12 21:24:15 +0200

    -

---
+++ src/main/java/org/example/BridgePatternTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+
+public class BridgePatternTest {
+    public static void main(String[] args) {
+        Device smartTV = new SmartTV();
+        SmartRemote smartRemote = new SmartRemote(smartTV);
+
+        smartRemote.power();
+        smartRemote.volumeUp();
+        smartRemote.volumeDown();
+        smartRemote.channelUp();
+        smartRemote.channelDown();
+
+        ((SmartTV) smartTV).browseInternet();
+        smartRemote.voiceControl("open YouTube");
+        smartRemote.voiceControl("play music");
+    }
+}
---
+++ src/main/java/org/example/SmartRemote.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+public class SmartRemote extends BasicRemote {
+    public SmartRemote(Device device) {
+        super(device);
+    }
+
+    public void voiceControl(String command) {
+        if (device.isEnabled()) {
+            System.out.println("Executing voice command: " + command);
+            if (command.equalsIgnoreCase("open youtube")) {
+                System.out.println("Opening YouTube on SmartTV.");
+            } else {
+                System.out.println("Command not recognized.");
+            }
+        } else {
+            System.out.println("Cannot execute command. Device is OFF.");
+        }
+    }
+}
